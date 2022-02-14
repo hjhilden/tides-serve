02365048d3d12e51a77d9f0dd1904c2e4e8cd6ac
jonatan hilden <jonatan.hilden@gmail.com>
2022-02-14 16:03:52 +0200

    subpage
    add secondary pages

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 # Tide data server
+Read tides data from Irish tide gauge network and analyze / serve responses 
+(https://erddap.marine.ie/erddap/tabledap/IrishNationalTideGaugeNetwork.html)[https://erddap.marine.ie/erddap/tabledap/IrishNationalTideGaugeNetwork.html]
--- main.js
+++ main.js
@@ -6,14 +6,14 @@ const today = new Date();
 	today.setMinutes(-15)
 	const todayString = today.toISOString()
 
-	const url = `https://erddap.marine.ie/erddap/tabledap/IrishNationalTideGaugeNetwork.json?time%2Cstation_id%2Cdatasourceid%2CWater_Level_OD_Malin&time%3E=${todayString}&station_id=%22Galway%20Port%22`
+	const jsonUrl = `https://erddap.marine.ie/erddap/tabledap/IrishNationalTideGaugeNetwork.json?time%2Cstation_id%2Cdatasourceid%2CWater_Level_OD_Malin&time%3E=${todayString}&station_id=%22Galway%20Port%22`
 let ODWaterLevelValue, isOn
 
 // const response = await fetch('https://github.com/');
 // const body = await response.text();
 
 
-fetch(url)
+fetch(jsonUrl)
 .then(res => res.json())
 .then(out =>
 			{
@@ -25,21 +25,37 @@ fetch(url)
 
 .catch(err => err);
 
+const url=require('url');
 
-const port = 3000,
+const port = process.env.PORT || 3000,
    http = require("http"),
    app = http.createServer((request, response) => {
+    var pathname=url.parse(request.url).pathname;
+
+
+
      console.log("Received an incoming request!");
      response.writeHead(StatusCodes.OK, {
        "Content-Type": "text/html"
      });
   
      let responseMessage = `<h1>Hello, let me tell you about the tides in ${name}! The water level is now ${ODWaterLevelValue} and the value is thus ${isOn}</h1>`;
-     response.write(responseMessage);
-     response.end();
+    //  response.write(responseMessage);
+    //  response.end();
      console.log(`Sent a response : ${responseMessage}`);
+
+     switch(pathname){
+      case '/station_id_19':
+        response.end(isOn);
+      break;
+      default:
+         response.write(responseMessage);
+         response.end('default');
+      break;
+  }
+
    });
   
- app.listen(process.env.PORT || port);
+ app.listen(port);
  console.log(`The server has started and is listening on port number:
  ${port}`);
